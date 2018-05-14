17c55bb7c5ed6e13f4594770f213a10b5605a162
Ryan Shin <Ryan@Ryans-MacBook-Pro-2.local>
2018-05-14 15:15:38 -0700

    refactor controller

--- js/controller.js
+++ js/controller.js
@@ -2,12 +2,14 @@ class Controller{
     constructor(Perseus){
         this.Perseus = Perseus;
         this.game = Perseus.game;
-        this.controlState = 'default';
+        this.state = 'default';
         this.cursors = this.game.input.keyboard.createCursorKeys();
         this.pointer = this.game.input.mousePointer;
         this.keys = {};
         this.keys.A = this.game.input.keyboard.addKey(Phaser.KeyCode.A);
         this.keys.M = this.game.input.keyboard.addKey(Phaser.KeyCode.M);
+        this.keys.B = this.game.input.keyboard.addKey(Phaser.KeyCode.B);
+        this.keys.F = this.game.input.keyboard.addKey(Phaser.KeyCode.F);
         this.keys.X = this.game.input.keyboard.addKey(Phaser.KeyCode.X);
         this.selectedObjects = [];
         this.lastPointerState; 
@@ -25,81 +27,84 @@ class Controller{
         obj.drawSelectionCircle();
     }
 
-
-    update(){
-        if(this.Perseus.graphics.selectionBox){
-            this.Perseus.graphics.selectionBox.destroy();
-        }
-        this.Perseus.graphics.selectionBox = this.game.add.graphics();
-        this.Perseus.graphics.selectionBox.lineStyle(2, 0xFFFFFF, 1);
-
+    cameraPan(){
         if(this.cursors.up.isDown || this.game.camera.height - this.pointer.position.y >= this.game.camera.height-32){
             let temp = this.game.camera.y; 
-            console.log("panning up");
             this.game.camera.y -= 10;
         }
         if(this.cursors.down.isDown || this.game.camera.height - this.pointer.position.y <= 32){
             let temp = this.game.camera.y;
-            console.log("panning down");
             this.game.camera.y += 10;
         }
         if(this.cursors.left.isDown || (this.game.camera.width - this.pointer.position.x) >= this.game.camera.width-32 ){
             let temp = this.game.camera.x; 
-            console.log("panning left");
             this.game.camera.x -= 10;
         }
         if(this.cursors.right.isDown || (this.game.camera.width - this.pointer.position.x) <= 32 ){
             let temp = this.game.camera.x; 
-            console.log("panning right");
             this.game.camera.x += 10;
         }
+    }
+
+    update(){
+        if(this.selectionBox){
+            this.selectionBox.destroy();
+        }
+
+        this.selectionBox = this.game.add.graphics();
+        this.selectionBox.lineStyle(2, 0xFFFFFF, 1);
+
+        this.cameraPan();
 
         if(this.keys.A.isDown){
             if (this.pointer.isDown == false){
-                this.controlState = 'attack';
-                console.log(this.controlState);
+                this.state = 'attack';
+                console.log(this.state);
                 console.log('input attack command');
             }
         }
         if(this.keys.M.isDown){
             if (this.pointer.isDown == false){
-                this.controlState = 'move';
+                this.state = 'move';
                 console.log('input move command');
 
             }
         }
         // Cancel command ( no right click yet)
         if (this.keys.X.isDown){
-            this.controlState = 'default';
+            this.state = 'default';
             console.log('controller state is default');
         }
 
+        // if (this.state == default && this.pointer.isDown == true){
+        // this.state = "pointerHold";
+        // }
         if (this.wasDown == false) {
             if (this.pointer.isDown == true){
-                if (this.controlState == 'default'){
+                if (this.state == 'default'){
                     console.log('starting selection box');
                     this.boxStartPos = this.pointer.positionDown; 
                     console.log(this.pointer.positionDown);
-                } else if (this.controlState == 'attack'){
+                } else if (this.state == 'attack'){
                     this.selectedObjects.forEach((obj) => {
-                        obj.move(this.pointer.positionDown.x, this.pointer.positionDown.y);
-                        this.controlState = 'default';
+                        obj.attack(this.pointer.positionDown.x, this.pointer.positionDown.y);
                     }, this);
-                } else if (this.controlState == 'move'){
+                    this.state = 'default';
+                } else if (this.state == 'move'){
                     this.selectedObjects.forEach((obj) => {
                         console.log('moving');
                         obj.move(this.pointer.positionDown.x, this.pointer.positionDown.y);
-                        this.controlState = 'default';
+                        this.state = 'default';
                     }, this);
                 }
             }
         } if (this.wasDown == true) {
             if (this.pointer.isDown == false){
-                if (this.controlState == 'default'){
+                if (this.state == 'default'){
                     console.log('finishing selection box');
                     this.boxEndPos = this.pointer.positionUp;
                     console.log(this.boxEndPos);
-                    this.Perseus.graphics.selectionBox.drawRect(Math.min(this.boxStartPos.x, this.boxEndPos.x) + this.game.camera.x, Math.min(this.boxStartPos.y, this.boxEndPos.y)+ this.game.camera.y, Math.abs(this.boxEndPos.x-this.boxStartPos.x), Math.abs(this.boxEndPos.y-this.boxStartPos.y));
+                    this.selectionBox.drawRect(Math.min(this.boxStartPos.x, this.boxEndPos.x) + this.game.camera.x, Math.min(this.boxStartPos.y, this.boxEndPos.y)+ this.game.camera.y, Math.abs(this.boxEndPos.x-this.boxStartPos.x), Math.abs(this.boxEndPos.y-this.boxStartPos.y));
 
                     this.selectedObjects = [];
                     this.selectionCircles.forEach((circle)=>{
@@ -123,10 +128,10 @@ class Controller{
                 }
             }
             else if (this.pointer.isDown == true){
-                if (this.controlState == 'default'){
+                if (this.state == 'default'){
                     //draw rectangle
                     this.boxEndPos = this.pointer.position;
-                    this.Perseus.graphics.selectionBox.drawRect(Math.min(this.boxStartPos.x, this.boxEndPos.x) + this.game.camera.x, Math.min(this.boxStartPos.y, this.boxEndPos.y) + this.game.camera.y, Math.abs(this.boxEndPos.x-this.boxStartPos.x), Math.abs(this.boxEndPos.y-this.boxStartPos.y));
+                    this.selectionBox.drawRect(Math.min(this.boxStartPos.x, this.boxEndPos.x) + this.game.camera.x, Math.min(this.boxStartPos.y, this.boxEndPos.y) + this.game.camera.y, Math.abs(this.boxEndPos.x-this.boxStartPos.x), Math.abs(this.boxEndPos.y-this.boxStartPos.y));
                 }
             }
         }
