6720de5cf7b6c081f1fa9b0088dbe90e353ea27c
Ryan Shin <Ryan@Ryans-MacBook-Pro-2.local>
2018-05-14 15:51:32 -0700

    fix attack state

--- js/controller.js
+++ js/controller.js
@@ -21,10 +21,41 @@ class Controller{
     }
 
     select(obj){
-        this.selectionCircles.forEach((elem)=> {elem.destroy();})
+        if (this.state == 'default'){
+            this.selectionCircles.forEach((elem)=> {elem.destroy();});
+            this.selectedObjects = [];
+            this.selectedObjects.push(obj);
+            obj.drawSelectionCircle();
+        } else if (this.state == 'attack'){
+            this.selectedObjects.forEach( (elem) => {
+                elem.attack(obj);
+            });
+            this.state = 'default';
+        }
+    }
+
+    selectInBox(){
+        this.selectionBox.drawRect(Math.min(this.boxStartPos.x, this.boxEndPos.x) + this.game.camera.x, Math.min(this.boxStartPos.y, this.boxEndPos.y)+ this.game.camera.y, Math.abs(this.boxEndPos.x-this.boxStartPos.x), Math.abs(this.boxEndPos.y-this.boxStartPos.y));
+
         this.selectedObjects = [];
-        this.selectedObjects.push(obj);
-        obj.drawSelectionCircle();
+        this.selectionCircles.forEach((circle)=>{
+            circle.destroy();
+        });
+        // Select objects
+        this.Perseus.objects.forEach(function(obj){
+            console.log(obj.sprite.x, obj.sprite.y);
+            // Put this logic in function.
+            if(this.boxStartPos.x <= obj.sprite.x  && obj.sprite.x <= this.boxEndPos.x 
+                    || this.boxEndPos.x <= obj.sprite.x && obj.sprite.x <= this.boxStartPos.x){
+                if (this.boxStartPos.y <= obj.sprite.y && obj.sprite.y <= this.boxEndPos.y
+                        || this.boxEndPos.y <= obj.sprite.y && obj.sprite.y <= this.boxStartPos.y){
+                    console.log("this is inside foreach");
+                    this.selectedObjects.push(obj);
+                    console.log(this.selectedObjects);
+                    obj.drawSelectionCircle();
+                }
+            }
+        }, this);
     }
 
     cameraPan(){
@@ -47,6 +78,7 @@ class Controller{
     }
 
     update(){
+        console.log(this.state);
         if(this.selectionBox){
             this.selectionBox.destroy();
         }
@@ -56,19 +88,15 @@ class Controller{
 
         this.cameraPan();
 
-        if(this.keys.A.isDown){
-            if (this.pointer.isDown == false){
-                this.state = 'attack';
-                console.log(this.state);
+        if(this.state != 'pointerHold'){
+            if(this.keys.A.isDown){
                 console.log('input attack command');
+                this.state = 'attack';
             }
         }
         if(this.keys.M.isDown){
-            if (this.pointer.isDown == false){
-                this.state = 'move';
-                console.log('input move command');
-
-            }
+            this.state = 'move';
+            console.log('input move command');
         }
         // Cancel command ( no right click yet)
         if (this.keys.X.isDown){
@@ -76,66 +104,41 @@ class Controller{
             console.log('controller state is default');
         }
 
-        // if (this.state == default && this.pointer.isDown == true){
-        // this.state = "pointerHold";
-        // }
-        if (this.wasDown == false) {
+        if (this.state == 'default'){
             if (this.pointer.isDown == true){
-                if (this.state == 'default'){
-                    console.log('starting selection box');
-                    this.boxStartPos = this.pointer.positionDown; 
-                    console.log(this.pointer.positionDown);
-                } else if (this.state == 'attack'){
-                    this.selectedObjects.forEach((obj) => {
-                        obj.attack(this.pointer.positionDown.x, this.pointer.positionDown.y);
-                    }, this);
-                    this.state = 'default';
-                } else if (this.state == 'move'){
-                    this.selectedObjects.forEach((obj) => {
-                        console.log('moving');
-                        obj.move(this.pointer.positionDown.x, this.pointer.positionDown.y);
-                        this.state = 'default';
-                    }, this);
-                }
-            }
-        } if (this.wasDown == true) {
-            if (this.pointer.isDown == false){
-                if (this.state == 'default'){
-                    console.log('finishing selection box');
-                    this.boxEndPos = this.pointer.positionUp;
-                    console.log(this.boxEndPos);
-                    this.selectionBox.drawRect(Math.min(this.boxStartPos.x, this.boxEndPos.x) + this.game.camera.x, Math.min(this.boxStartPos.y, this.boxEndPos.y)+ this.game.camera.y, Math.abs(this.boxEndPos.x-this.boxStartPos.x), Math.abs(this.boxEndPos.y-this.boxStartPos.y));
-
-                    this.selectedObjects = [];
-                    this.selectionCircles.forEach((circle)=>{
-                        circle.destroy();
-                    });
-                    // Select objects
-                    this.Perseus.objects.forEach(function(obj){
-                        console.log(obj.sprite.x, obj.sprite.y);
-                        // Put this logic in function.
-                        if(this.boxStartPos.x <= obj.sprite.x  && obj.sprite.x <= this.boxEndPos.x 
-                                || this.boxEndPos.x <= obj.sprite.x && obj.sprite.x <= this.boxStartPos.x){
-                            if (this.boxStartPos.y <= obj.sprite.y && obj.sprite.y <= this.boxEndPos.y
-                                    || this.boxEndPos.y <= obj.sprite.y && obj.sprite.y <= this.boxStartPos.y){
-                                console.log("this is inside foreach");
-                                this.selectedObjects.push(obj);
-                                console.log(this.selectedObjects);
-                                obj.drawSelectionCircle();
-                            }
-                        }
-                    }, this);
-                }
-            }
-            else if (this.pointer.isDown == true){
-                if (this.state == 'default'){
-                    //draw rectangle
-                    this.boxEndPos = this.pointer.position;
-                    this.selectionBox.drawRect(Math.min(this.boxStartPos.x, this.boxEndPos.x) + this.game.camera.x, Math.min(this.boxStartPos.y, this.boxEndPos.y) + this.game.camera.y, Math.abs(this.boxEndPos.x-this.boxStartPos.x), Math.abs(this.boxEndPos.y-this.boxStartPos.y));
-                }
+                console.log('starting selection box');
+                this.boxStartPos = this.pointer.positionDown; 
+                console.log(this.pointer.positionDown);
             }
         }
-        this.wasDown = this.pointer.isDown;
+        if (this.state == 'move'){
+            if (this.pointer.isDown == true){
+                this.selectedObjects.forEach((obj) => {
+                    console.log('moving');
+                    obj.move(this.pointer.positionDown.x, this.pointer.positionDown.y);
+                    this.state = 'default';
+                }, this);
+            }
+        } 
+        if (this.state == 'attack'){
+            //wait for click event
+        } 
+        if (this.state == 'pointerHold') {
+            if (this.pointer.isDown == false){
+                console.log('finishing selection box');
+                this.boxEndPos = this.pointer.positionUp;
+                console.log(this.boxEndPos);
+                this.selectInBox();
+                this.state = 'default';
+            } else {
+                //draw rectangle
+                this.boxEndPos = this.pointer.position;
+                this.selectionBox.drawRect(Math.min(this.boxStartPos.x, this.boxEndPos.x) + this.game.camera.x, Math.min(this.boxStartPos.y, this.boxEndPos.y) + this.game.camera.y, Math.abs(this.boxEndPos.x-this.boxStartPos.x), Math.abs(this.boxEndPos.y-this.boxStartPos.y));
+            }
+        }
+        if (this.state == 'default' && this.pointer.isDown){
+            this.state = 'pointerHold';
+        }
     }
 
     endWithSelect(obj){
@@ -143,5 +146,6 @@ class Controller{
         this.selectedObjects.push(obj);
         obj.drawSelectionCircle();
     }
+
 }
 export {Controller}
