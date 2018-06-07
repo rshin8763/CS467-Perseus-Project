91bbd4636ac604662c3b4c2c50f2329e09cb7d78
Catherine Kaethler <kaethlec@oregonstate.edu>
2018-06-06 23:05:44 -0700

    save

---
+++ js/saveGame.js
@@ -0,0 +1,122 @@
+import {Player} from './player.js';
+import {AI} from './ai.js';
+import {Resource} from './resource.js';
+
+/*****************************************************************************/
+							// GLOBALS // 
+/*****************************************************************************/
+
+// ******** GAME'S RESOURCES ********
+var savedMapResources = [];
+//	tag
+//	depleated or not
+
+// ******** PLAYER'S DATA ********
+var savedPlayerBuildings = [];
+//	Building = {
+//		name:
+//		hp:
+//	};
+
+var savedPlayerUnits = [];
+//	Units = {
+//		name:
+//		hp:
+//	};
+
+
+var savedPlayerStocks = [];	// ** LOAD LAST **
+//	Stock = {
+//		gold:
+//		wood:
+//	};
+
+// ******** AI DATA ********
+var savedAIBuildings = [];
+//	Building = {
+//		name:
+//		hp:
+//	};
+
+var savedAIUnits = [];
+//	Units = {
+//		name:
+//		hp:
+//	};
+
+var savedAIStocks = [];
+//	Stock = {
+//		gold:
+//		wood:
+//	};
+
+class SaveGame
+{
+	constructor(Perseus)
+	{
+		this.Perseus = Perseus;
+	}
+/*****************************************************************************/
+							// SAVE // 
+/*****************************************************************************/
+	
+	/*-----------------------------------------------------------------------*/
+	// MAP RESOURCES
+	SaveMapResources()
+	{
+
+	}
+
+	/*-----------------------------------------------------------------------*/
+	// PLAYER
+	SavePlayerBuildings()
+	{
+
+	}
+
+	SavePlayerUnits()
+	{
+
+	}
+
+	/*-----------------------------------------------------------------------*/
+	// AI
+	SaveAIBuildings()
+	{
+		var name, hp;
+		var Building;
+		for (var key in this.Perseus.AI.MyBuildings)
+		{
+			console.log("The name is: " + this.Perseus.AI.MyBuildings[key].kind)
+			//console.log("The hp is: " + this.Perseus.AI.MyBuildings[key].Unit.hp);
+		}
+	}
+
+	SaveAIUnits()
+	{
+
+	}
+
+/*****************************************************************************/
+							// LOAD // 
+/*****************************************************************************/
+
+	LoadMapResources()
+	{
+
+	}
+
+/*****************************************************************************/
+							// DEBUGGING // 
+/*****************************************************************************/
+
+/*****************************************************************************/
+							// MAIN // 
+/*****************************************************************************/
+	Main()
+	{
+
+	}
+}
+
+export {SaveGame}
